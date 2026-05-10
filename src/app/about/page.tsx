221e037a9762261b54ae9d5cfcb2b865c975606e
abdullahshahid836-ctrl <abdullahshahid836@gmail.com>
2026-05-10 14:54:15 +0500

    feat: added border radius to top-left, bottom-left and bottom-right grid corners

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -237,6 +237,9 @@ const AboutPage = () => {
 
               // Outer Perimeter Rounding
               let roundingClass = "";
+              if (i === 1) roundingClass = "rounded-tl-[8rem]";
+              if (i === 6) roundingClass = "rounded-bl-[8rem]";
+              if (i === 7) roundingClass = "rounded-br-[8rem]";
 
               return (
                 <div
